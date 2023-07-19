1a7401339bc799d8a25dbebb22aca501176201c5
Quen-Quen Potter <quenquen@outlook.com>
2023-07-19 15:58:37 -0300

    IcoNostr HelloWorld

---
+++ README.md
@@ -0,0 +1,38 @@
+# IcoNostr
+
+The IcoNostr is a free-to-use set that includes icons in solid style, made exclusively for the Nostr Community, packaged as webfont for easy use.
+
+You can add IcoNostr to your project by downloading the assets directly from Github. Source SVG files are also available.
+
+The icons are licenced using Creative Commons BY-SA-4.0 and fonts by MIT licence, allowing for use in commercial projects.
+
+## Getting Started
+
+How does it work? 
+
+1.Start by downloading the package.
+
+2.Include the font stylesheet into the header of your page:
+
+```<link rel="stylesheet" href="iconostr.css">```
+
+3.Add the nstr markup ```<i class="nstr-iconname"></i>```.
+
+
+## Using Icons
+
+Display the icons by using classes on the ```<i>``` html tag. For example, you can use the nostr icon by using the following markup:
+
+```<i class="nstr-nostr"></i>```
+
+## License
+
+
+### Font
+
+Licensed under [MIT license](http://opensource.org/licenses/mit-license.html).
+
+### Icons
+
+Icons are distributed under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/).
+
---
+++ iconostr.js
@@ -0,0 +1,37 @@
+function switchMode(el) {
+  const bodyClass = document.body.classList;
+  bodyClass.contains('dark')
+    ? (el.innerHTML = '<i class="nstr-sun" style="font-size: 36px;"></i>', bodyClass.remove('dark'))
+    : (el.innerHTML = '<i class="nstr-moon" style="font-size: 36px;"></i>', bodyClass.add('dark')); 
+}
+
+if (!('boxShadow' in document.body.style)) {
+    document.body.setAttribute('class', 'noBoxShadow');
+}
+
+document.body.addEventListener("click", function(e) {
+    var target = e.target;
+    if (target.tagName === "INPUT" &&
+        target.getAttribute('class').indexOf('liga') === -1) {
+        target.select();
+    }
+});
+
+(function() {
+    var fontSize = document.getElementById('fontSize'),
+        testDrive = document.getElementById('testDrive'),
+        testText = document.getElementById('testText');
+    function updateTest() {
+        testDrive.innerHTML = testText.value || String.fromCharCode(160);
+        if (window.icomoonLiga) {
+            window.icomoonLiga(testDrive);
+        }
+    }
+    function updateSize() {
+        testDrive.style.fontSize = fontSize.value + 'px';
+    }
+    fontSize.addEventListener('change', updateSize, false);
+    testText.addEventListener('input', updateTest, false);
+    testText.addEventListener('change', updateTest, false);
+    updateSize();
+}());
